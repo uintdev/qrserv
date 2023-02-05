75048df8b8f9468b1507af34a88efefd52962db3
Andre <core@uint.dev>
2023-02-05 23:49:43 +0000

    Add tablet landscape support (#7)
    Right out the gate: this is not the perfect solution, but this appears the best way to go without creating separate builds (this might be fine for app bundles, but not so much for APK files that are generally offered).

--- android/app/src/main/kotlin/dev/uint/qrserv/MainActivity.kt
+++ android/app/src/main/kotlin/dev/uint/qrserv/MainActivity.kt
@@ -4,7 +4,32 @@ import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+import android.os.Bundle
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+
 class MainActivity: FlutterFragmentActivity() {
+    @SuppressLint("SourceLockedOrientationActivity")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (this.getResources().getBoolean(R.bool.tablet_mode)) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR
+        } else {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+        super.onCreate(savedInstanceState);
+    }
+
+    @SuppressLint("SourceLockedOrientationActivity")
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        if (this.getResources().getBoolean(R.bool.tablet_mode)) {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR
+        } else {
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+        super.onConfigurationChanged(newConfig);
+    }
+
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine)
     }
