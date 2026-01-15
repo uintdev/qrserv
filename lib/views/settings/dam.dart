718fdb0af772ccd2a30db021661a8d7f454efcd0
uintdev <core@uint.dev>
2026-01-15 20:00:47 +0000

    Remove DAM state toasts

--- lib/views/settings/dam.dart
+++ lib/views/settings/dam.dart
@@ -1,8 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:qrserv/components/preferences.dart';
-import '../../l10n/generated/app_localizations.dart';
-import 'package:oktoast/oktoast.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import '../../components/filemanager.dart';
 
@@ -25,9 +23,6 @@ class DAM {
   }
 
   Future<void> toggle(BuildContext context, StateSetter setState) async {
-    !FileManager.directAccessMode
-        ? showToast(AppLocalizations.of(context)!.dam_state_enabled)
-        : showToast(AppLocalizations.of(context)!.dam_state_disabled);
     setState(() async {
       FileManager.directAccessMode = !FileManager.directAccessMode;
       await Preferences().write(
