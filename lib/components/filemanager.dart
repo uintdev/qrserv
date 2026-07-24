a73769ab0c02059fb4edd12673c0cb09911f62af
uintdev <core@uint.dev>
2026-07-24 20:46:23 +0100

    Remove unused import

--- lib/components/filemanager.dart
+++ lib/components/filemanager.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:flutter/material.dart';
 import '../l10n/generated/app_localizations.dart';
