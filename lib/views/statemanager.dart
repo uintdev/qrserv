03ffc6b88f71c0db840e40d65b018c5706bdb95f
uintdev <core@uint.dev>
2026-09-05 23:07:32 +0100

    Remove unused import

--- lib/views/statemanager.dart
+++ lib/views/statemanager.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:io';
 import 'package:material_ui/material_ui.dart';
 import '../l10n/generated/app_localizations.dart';
 import 'package:qr_flutter/qr_flutter.dart';
