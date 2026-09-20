386aa3d523a08860069e092e95ee9e182736cb91
uintdev <core@uint.dev>
2026-09-21 00:07:55 +0100

    Use better random generator source

--- app/src/main/kotlin/dev/uint/qrserv/util/TokenGenerator.kt
+++ app/src/main/kotlin/dev/uint/qrserv/util/TokenGenerator.kt
@@ -1,16 +1,18 @@
 package dev.uint.qrserv.util
 
-import kotlin.random.Random
+import java.security.SecureRandom
 
 object TokenGenerator {
     private const val DEFAULT_CHARS =
         "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+    private val random = SecureRandom()
+
     fun generate(characters: String = DEFAULT_CHARS, length: Int = 32): String {
         if (length <= 0) return ""
         val sb = StringBuilder(length)
         repeat(length) {
-            sb.append(characters[Random.nextInt(characters.length)])
+            sb.append(characters[random.nextInt(characters.length)])
         }
         return sb.toString()
     }
